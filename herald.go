6b6b08534e0f2d5c2ca1399ec2f3f59598c52f17
Xianghu Zhao <xianghuzhao@gmail.com>
2019-08-25 18:41:58 +0800

    Separate herald to new file

---
+++ herald.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"log"
+	"reflect"
+	"sync"
+)
+
+// Trigger should send trigger events to the core
+type Trigger interface {
+	Start(context.Context, chan string)
+}
+
+// Runner will run jobs
+type Runner interface {
+	Execute(string)
+}
+
+type job struct {
+	trigger string
+	runner  string
+}
+
+type herald struct {
+	wg       sync.WaitGroup
+	triggers map[string]Trigger
+	runners  map[string]Runner
+	jobs     map[string]*job
+}
+
+func (h *herald) addTrigger(name string, t Trigger) {
+	h.triggers[name] = t
+}
+
+func (h *herald) addRunner(name string, r Runner) {
+	h.runners[name] = r
+}
+
+func (h *herald) addJob(name, trigger, runner string) {
+	h.jobs[name] = &job{
+		trigger: trigger,
+		runner:  runner,
+	}
+}
+
+func (h *herald) start(ctx context.Context) {
+	go func() {
+		h.wg.Add(1)
+		defer h.wg.Done()
+
+		var cases []reflect.SelectCase
+
+		cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ctx.Done())})
+
+		var triggerNames []string
+		triggerChanStart := len(cases)
+		for triggerName, t := range h.triggers {
+			param := make(chan string)
+
+			log.Printf("Start trigger %s...\n", triggerName)
+
+			go func(t Trigger) {
+				h.wg.Add(1)
+				defer h.wg.Done()
+				t.Start(ctx, param)
+			}(t)
+
+			triggerNames = append(triggerNames, triggerName)
+			cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(param)})
+		}
+
+		log.Println("Waiting for triggers...")
+
+		for {
+			chosen, value, _ := reflect.Select(cases)
+
+			if chosen == 0 {
+				log.Println("Stop herald...")
+				return
+			}
+
+			triggerName := triggerNames[chosen-triggerChanStart]
+
+			log.Printf("Trigger active: \"%s\"\n", triggerName)
+
+			var runnerNames []string
+
+			for _, j := range h.jobs {
+				if j.trigger == triggerName {
+					runnerNames = append(runnerNames, j.runner)
+				}
+			}
+
+			for _, runnerName := range runnerNames {
+				log.Printf("Job execute with runner \"%s\"\n", runnerName)
+				go func(r Runner) {
+					h.wg.Add(1)
+					defer h.wg.Done()
+					r.Execute(value.Interface().(string))
+				}(h.runners[runnerName])
+			}
+		}
+	}()
+}
+
+func (h *herald) wait() {
+	h.wg.Wait()
+}
--- main.go
+++ main.go
@@ -5,109 +5,29 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"reflect"
-	"sync"
 	"syscall"
 	"time"
 )
 
-// Trigger should send trigger events to the core
-type Trigger interface {
-	Start(context.Context, chan string)
-}
-
-// Runner will run jobs
-type Runner interface {
-	Execute(string)
-}
-
-type job struct {
-	trigger Trigger
-	runner  Runner
-	param   chan string
-}
-
-type herald struct {
-	wg   sync.WaitGroup
-	jobs map[string]*job
-}
-
-func (h *herald) addJob(name string, j *job) {
-	h.jobs[name] = j
-}
-
-func (h *herald) start(ctx context.Context) {
-	go func() {
-		h.wg.Add(1)
-		defer h.wg.Done()
-
-		var names []string
-		var cases []reflect.SelectCase
-
-		cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ctx.Done())})
-
-		for name, j := range h.jobs {
-			j.param = make(chan string)
-
-			log.Printf("Start trigger %s...\n", name)
-
-			go func(j *job) {
-				h.wg.Add(1)
-				defer h.wg.Done()
-				j.trigger.Start(ctx, j.param)
-			}(j)
-
-			names = append(names, name)
-			cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(j.param)})
-		}
-
-		log.Println("Waiting for triggers...")
-
-		for {
-			chosen, value, _ := reflect.Select(cases)
-
-			if chosen == 0 {
-				log.Println("Stop herald...")
-				return
-			}
-
-			name := names[chosen-1]
-
-			log.Printf("Start to execute %s...\n", name)
-
-			go func() {
-				h.wg.Add(1)
-				defer h.wg.Done()
-				h.jobs[name].runner.Execute(value.Interface().(string))
-			}()
-		}
-	}()
-}
-
-func (h *herald) wait() {
-	h.wg.Wait()
-}
-
 func main() {
 	h := &herald{
-		jobs: make(map[string]*job),
+		triggers: make(map[string]Trigger),
+		runners:  make(map[string]Runner),
+		jobs:     make(map[string]*job),
 	}
 
-	j := &job{
-		trigger: &triggerTick{
-			interval: 2 * time.Second,
-		},
-		runner: &runnerLocal{},
-	}
-	h.addJob("first", j)
+	h.addTrigger("Trigger2s", &triggerTick{
+		interval: 2 * time.Second,
+	})
 
-	j2 := &job{
-		trigger: &triggerTick{
-			interval: 3 * time.Second,
-		},
-		runner: &runnerLocal{},
-	}
-	h.addJob("second", j2)
+	h.addTrigger("Trigger3s", &triggerTick{
+		interval: 3 * time.Second,
+	})
+
+	h.addRunner("RunnerPrint", &runnerLocal{})
+
+	h.addJob("Print2s", "Trigger2s", "RunnerPrint")
+	h.addJob("Print3s", "Trigger3s", "RunnerPrint")
 
 	ctx := context.Background()
 
